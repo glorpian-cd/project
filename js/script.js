b1ea0415187083abdd4423980727713d6999adc2
glorpian-cd <vadimprokazow@gmail.com>
2026-01-22 21:19:14 +0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,23 +1,8 @@
-const eras = document.querySelectorAll('.era');
 const timeline = document.querySelector('.timeline');
-const themes = [
-  'intro',
-  'arpanet',
-  'networks',
-  'www',
-  'web2',
-  'mobile',
-  'modern'
-];
+const startBtn = document.getElementById('startBtn');
 
-/* ===== ТЕМА ПРИ СКРОЛЛЕ ===== */
-timeline.addEventListener('scroll', () => {
-  const index = Math.round(timeline.scrollTop / window.innerHeight);
-  document.body.className = 'theme-' + themes[index];
-});
-
-/* ===== КНОПКА СТАРТ ===== */
-document.querySelector('.start-button')?.addEventListener('click', () => {
+/* ===== START BUTTON ===== */
+startBtn.addEventListener('click', () => {
   timeline.scrollTo({
     top: window.innerHeight,
     behavior: 'smooth'
